d244aeb9b41f2f01dc3977e2a321ba5750544ca8
Mowneash <125719771+Mowneash@users.noreply.github.com>
2024-07-04 12:48:29 +0530

    Create 2.Decade counters.v

---
+++ Sequential/Counters/2.Decade counters.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output reg [3:0] q);
+    always @(posedge clk)
+        if(reset | q==9)
+            q<=0;
+    else q<=q+1;
+    
+endmodule
